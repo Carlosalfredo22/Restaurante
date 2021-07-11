aecb1e29b54c07627070f1695054ffe60609a5b0
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2021-07-10 19:47:39 -0600

    Index2 de prueba completo

--- Index2.php
+++ Index2.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenido</title>
+    <link rel="stylesheet" type="text/css" href="../RestaurantSv/css/bootstrap.css">
 </head>
 <body>
     <H2>BIENVENIDOS AL RESTAURANTE SV</H2>
+    <button type="button" class="btn btn-outline-primary"><a href="../RestaurantSv/index.php">Regresar</a></button>
+
+
+    <!--boostrap  min js y popper min js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+      <script src="../RestaurantSv/js/bootstrap.min.js"></script>
 </body>
 </html>
